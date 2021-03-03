ef3d362c40d5dab1894b04c50756611e5d838ce4
Zacharias Steinmetz <steinmetz-z@uni-landau.de>
2021-03-03 15:09:17 +0100

    Add sample Raman spectrum (testdata) to /data

---
+++ R/raman_pe.R
@@ -0,0 +1,22 @@
+#' @title Sample Raman spectrum
+#'
+#' @description
+#' Polyethylene
+#'
+#' @format
+#' A data table containing 964 rows and 2 columns with information on:
+#' \tabular{ll}{
+#' wavenumber: \tab \cr
+#' absorbance: \tab \cr
+#' }
+#'
+#' @author
+#' author
+#'
+#' @references
+#' ref
+#'
+#' @docType data
+#' @keywords data
+#' @name raman_pe
+NULL
