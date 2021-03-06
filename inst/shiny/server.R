b68eaac16a154d0eeb9d97ed7661d670cbd03c69
Zacharias Steinmetz <steinmetz-z@uni-landau.de>
2021-03-06 20:20:47 +0100

    Call new functions

--- inst/shiny/server.R
+++ inst/shiny/server.R
@@ -6,7 +6,6 @@
 #Libraries ----
 library(shiny)
 library(ggplot2)
-library(smoother)
 library(dplyr)
 library(plotly)
 library(data.table)
@@ -18,25 +17,21 @@ library(DT)
 library(rdrop2)
 library(shinyhelper)
 library(hyperSpec)
-library(readr)
 library(tidyr)
 library(hexView)
 library(curl)
-#library(OpenSpecy)
-
 
 #Required Data ----
-load("data/Costs.RData")
-load("data/Donations.RData")
-load("data/FTIRLibrary.RData")
-load("data/FTIRLibraryMetadata.RData")
-load("data/RamanLibrary.RData")
-load("data/RamanLibraryMetadata.RData")
-load("data/FTIRLibraryPeak.RData")
-load("data/RamanLibraryPeak.RData")
-load("data/testdata.RData")
-WavelengthApprox <- seq(0,6000, by = 0.1)
+dir <- system.file("shiny", "data", package = "OpenSpecy")
+file.path(dir, "Costs.RData") %>% load()
+file.path(dir, "Donations.RData") %>% load()
+file.path(dir, "testdata.RData") %>% load()
 
+# Check if spectral library is present
+lib <- class(tryCatch(check_lib(), warning = function(w) {w}))
+if(any(lib == "warning")) get_lib()
+
+spec_lib <- load_lib()
 
 #Check for Auth Tokens and setup ----
 droptoken <- file.exists("data/droptoken.rds")
@@ -45,83 +40,6 @@ outputDir <- "Spectra"
 
 if(droptoken){
   drop_auth(rdstoken = "data/droptoken.rds")
-}
-
-#Required functions ----
-
-human_timestamp <- function() {
-  format(Sys.time(), "%Y%m%d-%H%M%OS")
-}
-
-#Function adjusts spectra with negative intensity values so that all intensity values are greater than 1.
-adjust_negative <- function(x){
-  if(min(x) < 1){
-    x + abs(min(x)) + 1
-  }
-  else{
-    x
-  }
-}
-
-
-#Function is the imodpolyfit function described by Zhao 2007.
-iModPolyFit <- function(x, y, n) {
-  OriginalWavelengths <- x #Need the original wavelengths
-  dev_prev = 0# DEV_PREV is the standard deviation residuals for the last
-  # iteration of polyfit. Set initially to 0.
-  first_iter = TRUE
-  criteria_met = FALSE
-  while (!criteria_met) {
-
-    #Predict the intensity using the polynomial of specified length.
-    paramVector = lm(y~stats::poly(x,n, raw = TRUE))
-
-    residual = paramVector$residuals
-
-    mod_poly = paramVector$fitted.values
-
-    dev_curr = sd(residual)
-
-    #Remove peaks.
-    if (first_iter){
-      Peaks <- c()
-      for (i in 1:length(y)) {
-        if (y[i] > mod_poly[i] + dev_curr){
-          Peaks <- c(Peaks,i)
-        }
-      }
-      y <- y[-Peaks]
-      mod_poly = mod_poly[-Peaks]
-      x <- x[-Peaks]
-      first_iter = FALSE
-    }
-
-    #Replace data with lower value if polynomial is lower.
-    for(j in 1:length(y)) {
-      if (mod_poly[j] + dev_curr > y[j]){
-        y[j] = y[j]
-      }
-      else {
-        y[j] = mod_poly[j]
-      }
-    }
-
-    #Test criteria.
-    criteria_met <- abs((dev_curr - dev_prev)/dev_curr) <= 0.05
-
-    #Approximate the intensity back to the original wavelengths, allows below the peak to be interpolated.
-    if(criteria_met) {
-      return(unname(unlist(approx(x, y, xout = OriginalWavelengths, rule = 2, method = "linear", ties=mean)[2])))
-    }
-
-    #Update previous residual metric.
-    dev_prev = dev_curr
-  }
-}
-
-#Min max normalization
-minmax <- function(x) {
-  (x - min(x))/(max(x)-min(x))
 }
 
 # This is the actual server functions, all functions before this point are not reactive.
@@ -204,7 +122,9 @@ server <- shinyServer(function(input, output, session) {
   #Compile metadata input
   formData <- reactive({
     data <- sapply(fieldsAll[1:(length(fieldsAll)-1)], function(x) input[[x]])
-    data <- c(data, paste(input[[fieldsAll[length(fieldsAll)]]][1], input[[fieldsAll[length(fieldsAll)]]][2], sep = "_"), timestamp = human_timestamp())
+    data <- c(data, paste(input[[fieldsAll[length(fieldsAll)]]][1],
+                          input[[fieldsAll[length(fieldsAll)]]][2], sep = "_"),
+              timestamp = human_timestamp())
     data
   })
 
@@ -224,166 +144,107 @@ server <- shinyServer(function(input, output, session) {
 
 
   #Read in data when uploaded based on the file type----
-  preprocesseddata <- reactive({
+  preprocessed_data <- reactive({
     req(input$file1)
     inFile <- input$file1
     filename <- tolower(as.character(inFile$datapath))
 
-    shiny::validate(shiny::need(any(c(endsWith(filename, ".jdx"), endsWith(filename, ".spc"), endsWith(filename, ".spa"), endsWith(filename, ".0"), endsWith(filename, ".csv"))), "Uploaded data type is not currently supported please check help icon (?) and About tab for details on data formatting."))
+    shiny::validate(shiny::need(any(c(endsWith(filename, ".jdx"),
+                                      endsWith(filename, ".spc"),
+                                      endsWith(filename, ".spa"),
+                                      endsWith(filename, ".0"),
+                                      endsWith(filename, ".asp"),
+                                      endsWith(filename, ".csv"))),
+                                "Uploaded data type is not currently supported please check help icon (?) and About tab for details on data formatting."))
 
     if(endsWith(filename, ".jdx")){
-      jdx <- read.jdx(inFile$datapath)
-      data.frame(Wavelength = jdx@wavelength, Absorbance = as.numeric(unname(jdx@data$spc[1,])))
+      read_jdx(inFile$datapath)
     }
     else if(endsWith(filename, ".spc")){
-      spc <- read.spc(inFile$datapath)
-      data.frame(Wavelength = spc@wavelength, Absorbance = as.numeric(unname(spc@data$spc[1,])))
+      read_spc(inFile$datapath)
     }
     else if(endsWith(filename, ".spa")){
-      to.read = file(inFile$datapath, "rb");
-      lines <- read_table(inFile$datapath)
-      begining <- lines$`Spectral Data File`[grepl("Resolution", lines$`Spectral Data File`)]
-
-      start <- as.numeric(strsplit(begining, " ")[[1]][4])
-      end <- as.numeric(strsplit(begining, " ")[[1]][6])
-
-      # Read the start offset
-      seek(to.read, 386, origin="start");
-      startOffset <- readBin(to.read, "int", n=1, size=2);
-      # Read the length
-      seek(to.read, 390, origin="start");
-      readLength <- readBin(to.read, "int", n=1, size=2);
-
-      # seek to the start
-      seek(to.read, startOffset, origin="start");
-
-      # we'll read four byte chunks
-      floatCount <- readLength/4
-
-      # read all our floats
-      floatData <- c(readBin(to.read,"double",floatCount, size=4))
-
-      data.frame(Wavelength = seq(end, start, length = length(floatData)), Absorbance = floatData)
-
+      read_spa(inFile$datapath)
     }
     else if(endsWith(filename, ".0")){
-      file.name = inFile$datapath
-      pa <- hexView::readRaw(file.name, offset = 0, nbytes = file.info(file.name)$size, human = "char", size = 1, endian = "little")
-      pr <- pa$fileRaw
-
-      codes=c("ZFF","RES","SNM","DAT","LWN","FXV","LXV","NPT","MXY","MNY","END","TIM")
-
-      ## Get positions where the following parameters are found in the file
-      z <- grepRaw(codes[1],pr,all=TRUE)[1]+5
-      re <- grepRaw(codes[2],pr,all=TRUE)[1]+5
-      snm <- grepRaw(codes[3],pr,all=TRUE)[1]+7
-      dat <- grepRaw(codes[4],pr,all=TRUE)[1]+7
-      lwn <- grepRaw(codes[5],pr,all=TRUE)[1]+7
-      fx <- grepRaw(codes[6],pr,all=TRUE)[3]+7
-      lx <- grepRaw(codes[7],pr,all=TRUE)[3]+7
-      npt0 <- grepRaw(codes[8],pr,all=TRUE)[2]+3
-      npt1 <- grepRaw(codes[8],pr,all=TRUE)[3]+7
-      mxy <- grepRaw(codes[9],pr,all=TRUE)[1]+7
-      mny <- grepRaw(codes[10],pr,all=TRUE)[3]+7
-      end <- grepRaw(codes[11],pr,all=TRUE)+11
-      tim <- grepRaw(codes[12],pr,all=TRUE)+11
-
-      ## calculate end and start of each block:
-      offs <- sapply(5:10, function(x){end[x]})
-      byts <- diff(offs)
-      ZFF <- readRaw(file.name, offset=z, nbytes=4, human="int", size=2)[[5]][1]
-      RES <- readRaw(file.name, offset=re, nbytes=4, human="int", size=2)[[5]][1]
-      snm.lab.material <- blockString(readRaw(file.name, offset = snm, nbytes = 22, human = "char", size = 1, endian = "little"))
-
-      ## Get number of data points for each spectra data block
-      NPT0 <- readRaw(file.name, offset=npt0, nbytes=12, human="int", size=4)[[5]][2]
-      NPT1 <- readRaw(file.name, offset=npt1, nbytes=4, human="int", size=4)[[5]][1]
-      fxv <- readRaw(file.name, offset=fx, nbytes=16, human="real", size=8)[[5]][1] ## fxv:	Frequency of first point
-      lxv <- readRaw(file.name, offset=lx, nbytes=16, human="real", size=8)[[5]][1] ## lxv:	Frequency of last point
-      Wavenumbers <- rev(seq(lxv, fxv, (fxv-lxv)/(NPT1-1)))
-
-      ## Read all through all the data blocks inside the OPUS file:
-      nbytes1 <- NPT0*4 ## initial parameters
-      smxa <- c()
-      smna <- c()
-      nbytes.f <- NPT1*4
-      if(offs[1]<2000){
-        offs.f<-offs[3]
-      }
-
-      if(offs[1]>20000){
-        offs.f<-offs[2]
-      }
-
-      ## Selected spectra block
-      opus.p <- readRaw(file.name,width=NULL,offset=offs.f-4,nbytes=nbytes.f,human="real",size=4,endian="little")
-      spectra <- opus.p[[5]]
-
-      data.frame(Wavelength = Wavenumbers, Absorbance = spectra)
-
+      read_0(inFile$datapath)
+    }
+    else if(endsWith(filename, ".asp")){
+      read_asp(inFile$datapath)
     }
     else{
-      shiny::validate(need(all(c("Wavelength", "Absorbance") %in% colnames(fread(inFile$datapath))), "Uploaded data format is not currently supported please check help icon (?) and About tab for details on data formatting."))
+      csv <- data.frame(fread(inFile$datapath))
 
-      fread(inFile$datapath)
+      # Try to guess column names
+      col_names <- c(
+        names(csv)[grep("wav*", ignore.case = T, names(csv))][1L],
+        names(csv)[grep("(transmit*)|(reflect*)|(abs*)|(intens*)",
+                        ignore.case = T, names(csv))][1L]
+      )
+      out <- csv[col_names]
+      names(out) <- c("wavenumber", "intensity")
 
-
+      out
     }
 
   })
 
-  #Corrects spectral intensity units using the user specified correction ----
-  absorbance <- reactive({
-    if(input$IntensityCorr == "Transmittance"){log10(1/adjust_negative(preprocesseddata()$Absorbance))}
-    else if(input$IntensityCorr == "Reflectance"){(1-adjust_negative(preprocesseddata()$Absorbance))^2/(2*adjust_negative(preprocesseddata()$Absorbance))}
-    else{adjust_negative(preprocesseddata()$Absorbance)}
-  })
-
-  #Formatting the dataset for further processing ----
+  # Corrects spectral intensity units using the user specified correction ----
   data <- reactive({
-    preprocesseddata() %>%
-      mutate(AbsorbanceAdj = absorbance()) %>%
-      mutate(NormalizedIntensity = minmax(absorbance())) %>%
-      mutate(BaselineRemove = NormalizedIntensity) #This is here to allow the user to choose the original data or the baseline corrected data to use.
+    adjust_intensity(preprocessed_data(), type = tolower(input$IntensityCorr))
   })
 
-  #compute spectral resolution ----
+  # Compute spectral resolution ----
   SpectralResolution <- reactive({
-    (max(data()$Wavelength) - min(data()$Wavelength))/length(data()$Wavelength)
+    (max(data()$wavenumber) - min(data()$wavenumber)) /
+      length(data()$wavenumber)
   })
 
-  #All cleaning of the data happens here. Smoothing and Baseline removing. ----
-  baselinedata <- reactive({
-    testdata <- data() %>% dplyr::filter(Wavelength > input$MinRange & Wavelength < input$MaxRange)
+  # All cleaning of the data happens here. Smoothing and Baseline removing. ----
+  # TODO: Continue here
+  baseline_data <- reactive({
+    testdata <- data() %>% dplyr::filter(wavenumber > input$MinRange & wavenumber < input$MaxRange)
     test <-  nrow(testdata) < 3
-    if(test){
+    if(test) {
+      data() %>% {
+        if(input$smoother != 0) {
+          smooth_intensity(., p = input$smoother)
+        } else {.} } %>% {
+        if(input$baseline != 0) {
+          subtract_background(., degree = input$baseline)
+        } else {.} }
+      } else {
       data() %>%
-        mutate(Smoothed = if(input$smoother != 0) signal::filter(filt = sgolay(p = input$smoother, n = 11), x = AbsorbanceAdj) else AbsorbanceAdj) %>%
-        mutate(BaselineRemove = if(input$baseline != 0) Smoothed - iModPolyFit(Wavelength,Smoothed, input$baseline) else Smoothed) %>%
-        mutate(BaselineRemove = minmax(BaselineRemove))
-    }
-    else{
-      data() %>%
-      dplyr::filter(Wavelength > input$MinRange & Wavelength < input$MaxRange) %>%
-      mutate(Smoothed = if(input$smoother != 0) signal::filter(filt = sgolay(p = input$smoother, n = 11), x = AbsorbanceAdj) else AbsorbanceAdj) %>%
-      mutate(BaselineRemove = if(input$baseline != 0) Smoothed - iModPolyFit(Wavelength,Smoothed, input$baseline) else Smoothed) %>%
-      mutate(BaselineRemove = minmax(BaselineRemove))
-    }
+      dplyr::filter(wavenumber > input$MinRange & wavenumber < input$MaxRange) %>% {
+        if(input$smoother != 0) {
+          smooth_intensity(., p = input$smoother)
+        } else {.} } %>% {
+          if(input$baseline != 0) {
+            subtract_background(., degree = input$baseline)
+          } else {.} }
+      }
   })
 
-
-  #Create file view and preprocess view.
+  # Create file view and preprocess view.
   output$MyPlot <- renderPlotly({
     plot_ly(data(), type = 'scatter', mode = 'lines') %>%
-      add_trace(x = ~Wavelength, y = ~NormalizedIntensity, name = 'Uploaded Spectrum', line = list(color = 'rgba(240,236,19, 0.8)')) %>%
-      layout(yaxis = list(title = "Absorbance Intensity"), xaxis = list(title = "Wavenumber (1/cm)"), plot_bgcolor='rgb(17,0,73)', paper_bgcolor='black', font = list(color = '#FFFFFF'))
+      add_trace(x = ~wavenumber, y = ~intensity, name = 'Uploaded Spectrum',
+                line = list(color = 'rgba(240,236,19, 0.8)')) %>%
+      layout(yaxis = list(title = "Absorbance Intensity"),
+             xaxis = list(title = "Wavenumber (1/cm)"), plot_bgcolor='rgb(17,0,73)',
+             paper_bgcolor='black', font = list(color = '#FFFFFF'))
   })
   output$MyPlotB <- renderPlotly({
    plot_ly(type = 'scatter', mode = 'lines') %>%
-      add_trace(data = baselinedata(), x = ~Wavelength, y = ~BaselineRemove, name = 'Processed Spectrum', line = list(color = 'rgb(240,19,207)')) %>%
-      add_trace(data = data(), x = ~Wavelength, y = ~NormalizedIntensity, name = 'Uploaded Spectrum', line = list(color = 'rgba(240,236,19,0.8)')) %>% #Dark blue rgb(63,96,130) https://www.rapidtables.com/web/color/RGB_Color.html https://www.color-hex.com/color-names.html
-      layout(yaxis = list(title = "Absorbance Intensity"), xaxis = list(title = "Wavenumber (1/cm)"), plot_bgcolor='rgb(17,0,73)', paper_bgcolor='black', font = list(color = '#FFFFFF'))
-
+      add_trace(data = baseline_data(), x = ~wavenumber, y = ~intensity,
+                name = 'Processed Spectrum', line = list(color = 'rgb(240,19,207)')) %>%
+      add_trace(data = data(), x = ~wavenumber, y = ~intensity,
+                name = 'Uploaded Spectrum', line = list(color = 'rgba(240,236,19,0.8)')) %>%
+      #Dark blue rgb(63,96,130) https://www.rapidtables.com/web/color/RGB_Color.html https://www.color-hex.com/color-names.html
+      layout(yaxis = list(title = "Absorbance Intensity"),
+             xaxis = list(title = "Wavenumber (1/cm)"),
+             plot_bgcolor='rgb(17,0,73)', paper_bgcolor='black',
+             font = list(color = '#FFFFFF'))
   })
 
   #Choose which spectrum to use ----
@@ -392,7 +253,7 @@ server <- shinyServer(function(input, output, session) {
       data()
     }
     else if(input$Data == "processed"){
-      baselinedata()
+      baseline_data()
     }
   })
 
@@ -564,7 +425,7 @@ server <- shinyServer(function(input, output, session) {
       paste('data-', human_timestamp(), '.csv', sep='')
     },
     content = function(file) {
-      write.csv(baselinedata(), file)
+      write.csv(baseline_data(), file)
     }
   )
 
