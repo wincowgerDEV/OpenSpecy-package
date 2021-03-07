249e1f6275218dc061fef72041b55467bdb50546
Zacharias Steinmetz <steinmetz-z@uni-landau.de>
2021-03-07 08:49:25 +0100

    Fix error in example

--- R/data_normalization.R
+++ R/data_normalization.R
@@ -3,28 +3,33 @@
 #' @title Normalization of spectral data
 #'
 #' @description
-#' \code{adjust_negative()} converts numeric values \code{x < 1} into values
-#' >= 1, keeping absolute differences between values by shifting intensity values with the value of the smallest number.
+#' \code{adjust_negative()} converts numeric values \code{x} < 1 into values
+#' >= 1, keeping absolute differences between values by shifting intensity
+#' values with the value of the smallest number.
 #' \code{make_relative()} converts values \code{x} into relative values between
 #' 0 and 1 using the standard normalization equation.
 #' If \code{na.rm} is \code{TRUE}, missing values are removed before the
 #' computation proceeds.
 #'
 #' @details
-#' \code{adjust_negative()} is used in Open Specy to avoid errors that could arise from log transforming spectra when using the \code{adjust_intensity()} and other functions.
-#' \code {make_relative()} is used in Open Specy to retain the relative height proportions between spectra while avoiding the large numbers that can result from some spectral instruments. 
+#' \code{adjust_negative()} is used in Open Specy to avoid errors that could
+#' arise from log transforming spectra when using \code{\link{adjust_intensity}()}
+#' and other functions.
+#' \code{make_relative()} is used in Open Specy to retain the relative
+#' height proportions between spectra while avoiding the large numbers that can
+#' result from some spectral instruments.
 #'
 #' @param x a numeric vector or an \R object which is coercible to one by
 #' \code{as.vector(x, "numeric")}.
 #' @param na.rm logical. Should missing values be removed?
 #'
 #' @seealso
-#' \code{\link[base]{min}} for the calculation of minima
-#' \code{adjust_intensity()} for log transformation functions
+#' \code{\link[base]{min}()} for the calculation of minima;
+#' \code{\link{adjust_intensity}()} for log transformation functions
 #'
 #' @examples
 #' adjust_negative(c(-1000, -1, 0, 1, 10))
-#' make_relative(-2:10)
+#' make_relative(c(-2:10))
 #'
 #' @importFrom magrittr %>%
 #' @export
