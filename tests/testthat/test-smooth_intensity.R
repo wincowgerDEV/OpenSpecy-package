7062ff076d383bc694094fa5549f8b34a8f431fb
Zacharias Steinmetz <steinmetz-z@uni-landau.de>
2021-03-08 15:46:36 +0100

    Test that smooth_intensity() works

---
+++ tests/testthat/test-smooth_intensity.R
@@ -0,0 +1,13 @@
+test_that("smooth_intensity() works as expected", {
+  expect_silent(smt <- smooth_intensity(raman_hdpe, p = 3))
+  expect_identical(smt, smooth_intensity(raman_hdpe$wavenumber, raman_hdpe$intensity))
+  expect_identical(smt, smooth_intensity(intensity ~ wavenumber, raman_hdpe))
+  expect_equal(as.numeric(
+    round(cor(smt[2], smooth_intensity(raman_hdpe, p = 1)[2]), 4)
+    ), 0.9756, ignore_attr = F)
+  expect_s3_class(smt, "data.frame")
+  expect_equal(names(smt), c("wavenumber", "intensity"))
+  expect_equal(nrow(smt), nrow(raman_hdpe))
+  expect_equal(smt[1], raman_hdpe[1])
+  expect_equal(range(smt[2]), c(0, 1))
+})
