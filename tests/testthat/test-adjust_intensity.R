c11556ea3408b4c62b46477333877ae514b0a088
Zacharias Steinmetz <steinmetz-z@uni-landau.de>
2021-03-08 15:46:03 +0100

    Add comparison between input methods

--- tests/testthat/test-adjust_intensity.R
+++ tests/testthat/test-adjust_intensity.R
@@ -1,5 +1,10 @@
 test_that("adjust_intensity() works as expected", {
   expect_silent(adj <- adjust_intensity(raman_hdpe))
+  expect_identical(adj, adjust_intensity(raman_hdpe$wavenumber, raman_hdpe$intensity))
+  expect_identical(adj, adjust_intensity(intensity ~ wavenumber, raman_hdpe))
+  expect_equal(as.numeric(
+    cor(raman_hdpe[2], adjust_intensity(raman_hdpe)[2])
+  ), 1, ignore_attr = F)
   expect_s3_class(adj, "data.frame")
   expect_equal(names(adj), c("wavenumber", "intensity"))
   expect_equal(nrow(adj), nrow(raman_hdpe))
