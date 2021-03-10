fe430ad732ff831f08afdf88f81c75c564a7c4ef
Zacharias Steinmetz <steinmetz-z@uni-landau.de>
2021-03-10 16:56:13 +0100

    All manage_lib tests working

--- tests/testthat/test-manage_lib.R
+++ tests/testthat/test-manage_lib.R
@@ -6,13 +6,13 @@ test_that("get_lib() downloads test library", {
   )
 })
 
-# test_that("check_lib() finds test library", {
-#   expect_silent(check_lib(which = "test", type = c("metadata", "library")))
-#   expect_warning(check_lib(which = "test", type = c("peaks")))
-# })
-#
-# test_that("load_lib() works as expected", {
-#   expect_silent(tl <- load_lib(which = "test", type = c("metadata", "library")))
-#   expect_type(tl, "list")
-#   expect_identical(tl, test_lib, ignore_attr = T)
-# })
+test_that("check_lib() finds test library", {
+  expect_silent(check_lib(which = "test", type = c("metadata", "library")))
+  expect_warning(check_lib(which = "test", type = c("peaks")))
+})
+
+test_that("load_lib() works as expected", {
+  expect_silent(tl <- load_lib(which = "test", type = c("metadata", "library")))
+  expect_type(tl, "list")
+  expect_identical(tl, test_lib, ignore_attr = T)
+})
