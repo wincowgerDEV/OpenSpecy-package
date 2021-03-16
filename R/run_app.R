b202590e5df968d02c7017a700f52c9d001ab414
Zacharias Steinmetz <steinmetz-z@uni-landau.de>
2021-03-16 18:29:38 +0100

    Check for suggest packages

--- R/run_app.R
+++ R/run_app.R
@@ -7,7 +7,8 @@
 #' After running this function the Open Specy GUI should open in a separate
 #' window or in your computer browser.
 #'
-#' @param app_dir the app to run.
+#' @param app_dir the app to run; defaults to
+#' \code{system.file("shiny", package = "OpenSpecy")}.
 #' @param \dots arguments passed to \code{\link[shiny]{runApp}()}.
 #'
 #' @seealso
@@ -25,6 +26,13 @@ run_app <- function(app_dir = system.file("shiny", package = "OpenSpecy"),
   if (app_dir == "") {
     stop("Could not find app directory. Try reinstalling OpenSpecy.",
          call. = FALSE)
+  }
+
+  pkg <- c("config", "shinyjs", "shinythemes", "shinyhelper",
+            "shinyWidgets", "plotly", "data.table", "DT", "curl", "rdrop2")
+  for (p in pkg) {
+    if (!requireNamespace(package = p))
+      stop(paste0("run_app() requires package '", p, "'"), call. = F)
   }
 
   runApp(app_dir, ...)
