01b8319af99d09e0037c4d11d95586ec90348fa5
Zacharias Steinmetz <git@zsteinmetz.de>
2021-04-10 11:35:07 +0200

    Change to api.osf.io

--- tests/testthat/test-manage_lib.R
+++ tests/testthat/test-manage_lib.R
@@ -5,7 +5,7 @@ tmp <- file.path(tempdir(), "OpenSpecy-testthat")
 dir.create(tmp, showWarnings = F)
 
 test_that("stop if OSF not reachable", {
-  skip_if_not(is.null(curl::nslookup("osf.io", error = F)),
+  skip_if_not(is.null(curl::nslookup("api.osf.io", error = F)),
               message = "OSF is online")
 
   expect_error(load_lib(which = "test", type = c("metadata", "library"),
@@ -13,7 +13,7 @@ test_that("stop if OSF not reachable", {
 })
 
 test_that("get_lib() downloads test library", {
-  skip_if_offline(host = "osf.io")
+  skip_if_offline(host = "api.osf.io")
 
   expect_message(
     expect_output(get_lib(which = "test", type = c("metadata", "library"),
@@ -22,7 +22,7 @@ test_that("get_lib() downloads test library", {
 })
 
 test_that("check_lib() finds test library", {
-  skip_if_offline(host = "osf.io")
+  skip_if_offline(host = "api.osf.io")
 
   expect_silent(check_lib(which = "test", type = c("metadata", "library"),
                           path = tmp))
@@ -31,7 +31,7 @@ test_that("check_lib() finds test library", {
 })
 
 test_that("load_lib() works as expected", {
-  skip_if_offline(host = "osf.io")
+  skip_if_offline(host = "api.osf.io")
 
   expect_silent(tl <- load_lib(which = "test", type = c("metadata", "library"),
                                path = tmp))
