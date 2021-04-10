3934aa78cbde8c0a1508e33321a71b8e65f0b91f
Zacharias Steinmetz <git@zsteinmetz.de>
2021-04-10 09:43:17 +0200

    Skip tests if OSF cannot be reached

--- tests/testthat/test-manage_lib.R
+++ tests/testthat/test-manage_lib.R
@@ -5,6 +5,8 @@ tmp <- file.path(tempdir(), "OpenSpecy-testthat")
 dir.create(tmp, showWarnings = F)
 
 test_that("get_lib() downloads test library", {
+  skip_if_offline(host = "osf.io")
+
   expect_message(
     expect_output(get_lib(which = "test", type = c("metadata", "library"),
                           path = tmp))
@@ -12,6 +14,8 @@ test_that("get_lib() downloads test library", {
 })
 
 test_that("check_lib() finds test library", {
+  skip_if_offline(host = "osf.io")
+
   expect_silent(check_lib(which = "test", type = c("metadata", "library"),
                           path = tmp))
   expect_warning(check_lib(which = "test", type = c("peaks"),
@@ -19,6 +23,8 @@ test_that("check_lib() finds test library", {
 })
 
 test_that("load_lib() works as expected", {
+  skip_if_offline(host = "osf.io")
+
   expect_silent(tl <- load_lib(which = "test", type = c("metadata", "library"),
                                path = tmp))
   expect_type(tl, "list")
