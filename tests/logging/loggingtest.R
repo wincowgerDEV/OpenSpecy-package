e9528781da0f2f66e33d3d3e43830d46957b1d0d
Win Cowger <wincowger@gmail.com>
2021-04-30 20:00:32 -0700

    logfile check update

--- tests/logging/loggingtest.R
+++ tests/logging/loggingtest.R
@@ -4,4 +4,4 @@ database <- mongo(url = readLines("inst/shiny/.db_url"))
 
 alldata <- database$find('{}')
 
-#To test the loggit, remove the .dburl file momentarily and update the location of the library in conf
+#To test the loggit, remove the .dburl file momentarily and update the location of the library in conf and the logfile location in server
