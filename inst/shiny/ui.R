9bc033f49447496623c25fc5bc46ab369cca21d0
Win Cowger <wincowger@gmail.com>
2021-12-31 19:55:16 -0800

    pretty much completed the i18n for now. We need to do a larger overhaul that I am not ready to do yet to make this full on. Basically transition all translatable text into a json format and reference it by index in the code base. All of this essentially needs to be done server side to get things like BS to work.

--- inst/shiny/ui.R
+++ inst/shiny/ui.R
@@ -205,7 +205,7 @@ ui <- fluidPage(
                             column(6, img(src = "dancing.jpg", width = "100%")
                             ),
                             column(6,                            
-                                   h3(i18n$t("Financial Partners")),
+                                   h3(i18n$t("Monetary Partners")),
                                    panel(style = "overflow-y:scroll; max-height: 300px;  align: centre",
                                          
                                div(class = "jumbotron",
@@ -251,7 +251,7 @@ ui <- fluidPage(
                                h6("Susanne Brander (Oregon State University), Jeremy Conkle (TEXAS  A&M  UNIVERSITY  CORPUS  CHRISTI)")
                            )
                           ),
-                          h3(i18n$t("Effort Partners")),
+                          h3(i18n$t("In-Kind Partners")),
                           panel(style = "overflow-y:scroll; max-height: 300px;  align: centre",
                                 div(class = "jumbotron",
                                     style = "padding:0rem 1rem 0rem;
@@ -783,7 +783,7 @@ ui <- fluidPage(
                          ),
 
                          column(9,
-                                plotcontainerfunction(h4(id = "placeholder3", "Upload some data to get started..."), plotlyOutput("MyPlotC"),
+                                plotcontainerfunction(h4(id = "placeholder3", i18n$t("Upload some data to get started...")), plotlyOutput("MyPlotC"),
                                                       DT::dataTableOutput("eventmetadata")),
                                 style = bodyformat()
 
@@ -804,8 +804,8 @@ ui <- fluidPage(
 
 
               #Partner With Us tab ----
-              tabPanel("Partner With Us",
-                       titlePanel(h4("Help us reach our goal of revolutionizing spectroscopy.")),
+              tabPanel(i18n$t("Partner With Us"),
+                       titlePanel(h4(i18n$t("Help us reach our goal of revolutionizing spectroscopy."))),
                        fluidRow(
                          column(1),
                          column(9,
@@ -820,26 +820,26 @@ ui <- fluidPage(
                          column(1),
                                   column(3,
                                          plotcontainerfunction(
-                                         tags$h3("Donate Cash"),
+                                         tags$h3(i18n$t("Donate Cash")),
                                          icon = icon("shopping-cart"),
                                          img(src = "https://p.turbosquid.com/ts-thumb/rX/Wm1eqB/t5/currencysymbolsgoldensetc4dmodel000/jpg/1613802168/300x300/sharp_fit_q85/a31625492ce9c8009ab3e4281ad752006e1163ec/currencysymbolsgoldensetc4dmodel000.jpg", style = "padding:1rem; background-color:rgba(255,255,255, 0.9)", width = "100%"),
-                                         actionButton(inputId = "ab1", label = "Donate", style="padding:4px; font-size:200%", width = "100%",
+                                         actionButton(inputId = "ab1", label = i18n$t("Donate"), style="padding:4px; background-color: #2a9fd6; font-size:200%", width = "100%",
                                                       icon = icon("donate"),
                                                       onclick = "window.open('https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=wincowger@gmail.com&lc=US&item_name=Donation+to+Open+Specy&no_note=0&cn=&currency_code=USD&bn=PP-DonationsBF:btn_donateCC_LG.gif:NonHosted', '_blank')")
                                          )),
                                   column(3,
-                                         plotcontainerfunction(tags$h3("Buy From Swag Store"),
+                                         plotcontainerfunction(tags$h3(i18n$t("Buy From Swag Store")),
                                          img(src = "https://image.spreadshirtmedia.com/image-server/v1/products/T813A823PA3132PT17X42Y46D1038541132FS4033/views/1,width=650,height=650,appearanceId=823/updated-logo-for-open-specy-designed-by-alex-mcgoran.jpg", style = "padding:1rem; background-color:rgba(255,255,255, 0.9)", width = "100%"),
-                                         actionButton(inputId = "ab2", label = "Shop", style="padding:4px; font-size:200%", width = "100%",
+                                         actionButton(inputId = "ab2", label = i18n$t("Shop"), style="padding:4px; background-color: #2a9fd6; font-size:200%", width = "100%",
                                                       icon = icon("shopping-cart"),
                                                       onclick ="window.open('https://shop.spreadshirt.com/openspecy/all', '_blank')")
                                          )),
                                   column(3,
                                          plotcontainerfunction(
-                                           h2("Contribute time"),
+                                           h2(i18n$t("Contribute time")),
                                            #p(class = "lead", "We are looking for coders, moderators, spectroscopy experts, microplastic researchers, industry, government, and others to join the Open Specy team. Please contact Win at wincowger@gmail.com"),
                                            img(src = "https://health.sunnybrook.ca/wp-content/uploads/2020/02/healthy-hands-810x424.jpg", style = "padding:1rem; background-color:rgba(255,255,255, 0.9)", width = "100%"),
-                                           actionButton(inputId = "ab3", label = "Guidelines", style="padding:4px; font-size:200%", width = "100%",
+                                           actionButton(inputId = "ab3", label = i18n$t("Guidelines"), style="padding:4px; background-color: #2a9fd6; font-size:200%", width = "100%",
                                                         icon = icon("clock"),
                                                         onclick ="window.open('https://docs.google.com/document/d/1SaFgAYKsLbMSYdJClR5s42TyGmPRWihLQcf5zun_yfo/edit?usp=sharing', '_blank')")
                                          )
