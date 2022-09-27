50e1ba4efcaa9c9022bbce0754dfa3087cacfcae
Zacharias Steinmetz <git@zsteinmetz.de>
2022-09-27 15:48:36 +0200

    `run_app()` from GitHub
    - first implementations; needs further tweaking
    - get Shiny app files from https://github.com/wincowgerDEV/OpenSpecy-shiny
    - remove legacy files from package repo

--- R/run_app.R
+++ R/run_app.R
@@ -7,17 +7,16 @@
 #' After running this function the Open Specy GUI should open in a separate
 #' window or in your computer browser.
 #'
-#' @param app_dir the app to run; defaults to \code{"system"} pointing to
-#' \code{system.file("shiny", package = "OpenSpecy")}.
-#' @param path where to look for the local library files; defaults to
-#' \code{"system"} pointing to
-#' \code{system.file("extdata", package = "OpenSpecy")}.
+#' @param path to store the downloaded app files; defaults to \code{"system"}
+#' pointing to \code{system.file(package = "OpenSpecy")}.
 #' @param log logical; enables/disables logging to \code{\link[base]{tempdir}()}
+#' @param ref git reference; could be a commit, tag, or branch name. Defaults to
+#' "main". Only change this in case of errors.
 #' @param \dots arguments passed to \code{\link[shiny]{runApp}()}.
 #'
 #' @return
 #' This function normally does not return any value, see
-#' \code{\link[shiny]{runApp}()}.
+#' \code{\link[shiny]{runGitHub}()}.
 #'
 #' @examples
 #' \dontrun{
@@ -28,16 +27,12 @@
 #' Zacharias Steinmetz
 #'
 #' @seealso
-#' \code{\link[shiny]{runApp}()}
+#' \code{\link[shiny]{runGitHub}()}
 #'
-#' @importFrom shiny runApp shinyOptions
+#' @importFrom shiny runGitHub shinyOptions
 #' @importFrom utils installed.packages
 #' @export
-run_app <- function(app_dir = "system", path = "system", log = TRUE, ...) {
-  if (is.null(app_dir) || app_dir == "")
-    stop("Could not find app directory. Try reinstalling OpenSpecy.",
-         call. = FALSE)
-
+run_app <- function(path = "system", log = TRUE, ref = "main", ...) {
   pkg <- c("config", "shinyjs", "shinythemes", "shinyBS", "shinyWidgets",
            "plotly", "data.table", "DT", "curl", "rdrop2", "mongolite",
            "loggit")
@@ -47,15 +42,12 @@ run_app <- function(app_dir = "system", path = "system", log = TRUE, ...) {
                         paste(paste0("'", mpkg, "'"), collapse = ", "),
                         call. = F)
 
-  ad <- ifelse(app_dir == "system",
-               system.file("shiny", package = "OpenSpecy"),
-               app_dir)
-  wd <- ifelse(path == "system",
-               system.file("extdata", package = "OpenSpecy"),
+  dd <- ifelse(path == "system",
+               system.file(package = "OpenSpecy"),
                path)
 
   Sys.setenv(R_CONFIG_ACTIVE = "run_app")
 
-  shinyOptions(library_path = wd, log = log)
-  runApp(ad, ...)
+  shinyOptions(log = log)
+  runGitHub("OpenSpecy-shiny", "wincowgerDEV", destdir = dd, ref = ref, ...)
 }
