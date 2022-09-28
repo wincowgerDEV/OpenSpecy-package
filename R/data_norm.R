45bcc4657dd6cecb3c715cdec64b66555a47717a
Zacharias Steinmetz <git@zsteinmetz.de>
2022-09-28 15:22:23 +0200

    Add new functions for wavenumber adjustments

--- R/data_norm.R
+++ R/data_norm.R
@@ -1,33 +1,46 @@
 #' @rdname data_norm
 #'
-#' @title Normalization of spectral data
+#' @title Normalization and conversion of spectral data
 #'
 #' @description
-#' \code{adj_neg()} converts numeric values \code{x} < 1 into values
-#' >= 1, keeping absolute differences between values by shifting intensity
-#' values with the value of the smallest number.
-#' \code{make_rel()} converts values \code{x} into relative values between
+#' \code{adj_res()} and \code{conform_res()} are helper functions to conform
+#' wavenumbers to different spectral resolutions.
+#' \code{adj_neg()} converts numeric intensities \code{y} < 1 into values
+#' >= 1, keeping absolute differences between intensity values by shifting each
+#' value by the minimum intensity.
+#' \code{make_rel()} converts intensities \code{y} into relative values between
 #' 0 and 1 using the standard normalization equation.
 #' If \code{na.rm} is \code{TRUE}, missing values are removed before the
 #' computation proceeds.
 #'
 #' @details
-#' \code{adj_neg()} is used in Open Specy to avoid errors that could
-#' arise from log transforming spectra when using
-#' \code{\link{adj_intens}()} and other functions.
-#' \code{make_rel()} is used in Open Specy to retain the relative
-#' height proportions between spectra while avoiding the large numbers that can
-#' result from some spectral instruments.
+#' \code{adj_res()} and \code{conform_res()} are used in Open Specy to
+#' faciliate comparisons of spectra with different resolutions.
+#' \code{adj_neg()} is used to avoid errors that could arise from log
+#' transforming spectra when using \code{\link{adj_intens}()} and other
+#' functions.
+#' \code{make_rel()} is used to retain the relative height proportions between
+#' spectra while avoiding the large numbers that can result from some spectral
+#' instruments.
 #'
 #' @param x a numeric vector or an \R object which is coercible to one by
-#' \code{as.vector(x, "numeric")}; \code{x} should be \code{intensity} data.
+#' \code{as.vector(x, "numeric")}; \code{x} should contain the spectral
+#' wavenumbers.
+#' @param y a numeric vector containing the spectral intensities.
+#' @param res spectral resolution supplied to \code{fun}.
+#' @param fun the function to be applied to each element of \code{x}; defaults
+#' to \code{\link[base]{round}()} to round to a specific resolution \code{res}.
 #' @param na.rm logical. Should missing values be removed?
 #'
 #' @return
+#' \code{adj_res()} abd \code{conform_res()} return a numeric vector with
+#' resolution-conformed wavenumbers.
 #' \code{adj_neg()} and \code{make_rel()} return numeric vectors
-#' with the normalized data.
+#' with the normalized intensity data.
 #'
 #' @examples
+#' adj_res(seq(500, 4000, 4), 5)
+#' conform_res(seq(500, 4000, 4))
 #' adj_neg(c(-1000, -1, 0, 1, 10))
 #' make_rel(c(-1000, -1, 0, 1, 10))
 #'
@@ -35,22 +48,36 @@
 #' Win Cowger, Zacharias Steinmetz
 #'
 #' @seealso
-#' \code{\link[base]{min}()} for the calculation of minima;
+#' \code{\link[base]{min}()} and \code{\link[base]{round}()};
 #' \code{\link{adj_intens}()} for log transformation functions
 #'
 #' @importFrom dplyr %>%
 #' @export
-adj_neg <- function(x, na.rm = FALSE) {
-  if (min(x, na.rm = na.rm) < 1) {
-    x + min(x, na.rm = na.rm) %>% abs() + 1
+adj_res <- function(x, res = 1, fun = round) {
+  fun(x / res) * res
+}
+
+#' @rdname data_norm
+#'
+#' @export
+conform_res <- function(x, res = 5) {
+  seq(adj_res(min(x), res, ceiling), adj_res(max(x), res, floor), by = res)
+}
+
+#' @rdname data_norm
+#'
+#' @export
+adj_neg <- function(y, na.rm = FALSE) {
+  if (min(y, na.rm = na.rm) < 1) {
+    y + min(y, na.rm = na.rm) %>% abs() + 1
   } else {
-    x
+    y
   }
 }
 
 #' @rdname data_norm
 #'
 #' @export
-make_rel <- function(x, na.rm = FALSE) {
-  (x - min(x, na.rm = na.rm)) / (max(x, na.rm = na.rm) - min(x, na.rm = na.rm))
+make_rel <- function(y, na.rm = FALSE) {
+  (y - min(y, na.rm = na.rm)) / (max(y, na.rm = na.rm) - min(y, na.rm = na.rm))
 }
