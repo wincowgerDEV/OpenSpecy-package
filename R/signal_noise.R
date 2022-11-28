fad3150b4094857c04329944c0d2f2087690d64a
Zacharias Steinmetz <git@zsteinmetz.de>
2022-11-28 15:25:40 +0100

    Comment out new functions for now

--- R/signal_noise.R
+++ R/signal_noise.R
@@ -1,22 +1,22 @@
-
-vapply(map$spectra, function(x) {signal_noise(wavenumber = map$wavenumber, intensity = x)}, FUN.VALUE = numeric(1))
-intensity = map$spectra[[1]]
-
-signal_noise <- function(wavenumber, intensity, remove_min = 2200, remove_max = 2420, include_min = 1900, include_max = 2700, return = "signal_times_noise") {
-    include_values <- wavenumber >= include_min & wavenumber <= include_max
-    remove_values <- wavenumber >= remove_min & wavenumber <= remove_max 
-    signal = mean(intensity[!remove_values], na.rm = T)
-    noise = sd(intensity[!remove_values & include_values], na.rm = T)
-    if(return == "signal"){
-        return(signal)
-    }
-    if(return == "noise"){
-        return(noise)
-    }
-    if(return == "signal_times_noise"){
-        return(abs(signal*noise))
-    }
-    if(return == "signal_to_noise"){
-        return(abs(signal/noise))
-    }
-}
+#
+# vapply(map$spectra, function(x) {signal_noise(wavenumber = map$wavenumber, intensity = x)}, FUN.VALUE = numeric(1))
+# intensity = map$spectra[[1]]
+#
+# signal_noise <- function(wavenumber, intensity, remove_min = 2200, remove_max = 2420, include_min = 1900, include_max = 2700, return = "signal_times_noise") {
+#     include_values <- wavenumber >= include_min & wavenumber <= include_max
+#     remove_values <- wavenumber >= remove_min & wavenumber <= remove_max
+#     signal = mean(intensity[!remove_values], na.rm = T)
+#     noise = sd(intensity[!remove_values & include_values], na.rm = T)
+#     if(return == "signal"){
+#         return(signal)
+#     }
+#     if(return == "noise"){
+#         return(noise)
+#     }
+#     if(return == "signal_times_noise"){
+#         return(abs(signal*noise))
+#     }
+#     if(return == "signal_to_noise"){
+#         return(abs(signal/noise))
+#     }
+# }
