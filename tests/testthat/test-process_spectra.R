ade903fe75ebd1bf57930e7fde462fc40beef813
nickleong20 <nick@mooreplasticresearch.org>
2023-07-26 11:00:52 -0700

    added no error test

--- tests/testthat/test-process_spectra.R
+++ tests/testthat/test-process_spectra.R
@@ -3,3 +3,8 @@ test_that("process returns expected values",{
     expect_silent(processed <- process_spectra(data))
     expect_true(is_OpenSpecy(processed))
 })
+
+test_that("process runs without issue", {
+  data <- read_any(read_extdata("CA_tiny_map.zip"))
+  expect_no_error(process_spectra(data))
+})
