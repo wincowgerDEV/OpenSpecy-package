455e6a28d74abd6b73170fd482dbdec1d1b92529
nickleong20 <nick@mooreplasticresearch.org>
2023-08-02 12:36:50 -0700

    fixed expected result in test

--- tests/testthat/test-spec_res.R
+++ tests/testthat/test-spec_res.R
@@ -1,5 +1,5 @@
 data("raman_hdpe")
 
 test_that("spec_res() gives correct output", {
-  expect_equal(round(spec_res(raman_hdpe$wavenumber), 3), 3.005)
+  expect_equal(round(spec_res(raman_hdpe$wavenumber), 2), 2.54)
 })
