fec3b09b62a9a979a4a1a2de5fe651681c8a3fde
Zacharias Steinmetz <git@zsteinmetz.de>
2023-08-16 20:16:10 +0200

    Rename test file

--- tests/testthat/test-subtr_baseline.R
+++ tests/testthat/test-subtr_baseline.R
@@ -1,11 +1,11 @@
 data("raman_hdpe")
 
 test_that("subtr_baseline() works as expected", {
-  expect_silent(sb <- subtr_baseline(raman_hdpe, degree = 8))
-  expect_equal(as.numeric(
-    round(cor(sb$spectra$intensity,
-              subtr_baseline(raman_hdpe, degree = 1)$spectra$intensity), 4)
-  ), 0.9763, ignore_attr = F)
+  sb <- subtr_baseline(raman_hdpe, degree = 8) |> expect_silent()
+
+  cor(sb$spectra$intensity,
+      subtr_baseline(raman_hdpe, degree = 1)$spectra$intensity) |> round(4) |>
+    expect_equal(0.9763, ignore_attr = F)
   expect_s3_class(sb, "OpenSpecy")
   expect_equal(nrow(sb$spectra), nrow(raman_hdpe$spectra))
   expect_equal(sb$wavenumber, raman_hdpe$wavenumber)
