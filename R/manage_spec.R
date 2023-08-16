84dcd3026c075fd57efa60077a6e4215cd89c553
Zacharias Steinmetz <git@zsteinmetz.de>
2023-08-16 16:46:35 +0200

    S3 method and tests

--- R/manage_spec.R
+++ R/manage_spec.R
@@ -1,73 +1,92 @@
 #' @rdname manage_spec
-#'
 #' @title Manage spectral objects
 #'
 #' @description
-#' Functions for
+#' Functions for concatenating \code{OpenSpecy} objects.
 #'
-#' @details
-#' details
-#'
-#' @param objects a list object of class \code{OpenSpecy}.
-#' @param wavenumber_transform a numeric if providing your own wavenumbers or character argument of "common_range" depending on how you want to merge the wavenumbers between the spectra, NULL will be interpreted as the spectra are all the same wavenumber range, see details for more information.
-#' @param res defaults to \code{NULL}, the resolution you want the output wavenumbers to be.
+#' @param x a list of \code{OpenSpecy} objects.
+#' @param range a numeric providing your own wavenumber ranges or character
+#' argument called \code{"common"} to let \code{c_spec()} find the common
+#' wavenumber range of the supplied spectra. \code{NULL} will interpret the
+#' spectra having all the same wavenumber range.
+#' @param res defaults to \code{NULL}, the resolution you want the output
+#' wavenumbers to be.
+#' @param \ldots further arguments passed to submethods.
 #'
 #' @return
-#' A single \code{OpenSpecy} object. 
-#' 
+#' A single \code{OpenSpecy} object.
+#'
 #' @examples
-#' spectra <- lapply(c(read_extdata("raman_hdpe.csv"),  read_extdata("ftir_ldpe_soil.asp")), read_any)
-#' spectra2 <- c_spec(objects = spectra, wavenumber_transform = "common_range", res = 5)
-#' spectra3 <- c_spec(objects = spectra, wavenumber_transform = c(1000, 2000), res = 5)
-#' 
+#' spectra <- lapply(c(read_extdata("raman_hdpe.csv"),
+#'                     read_extdata("ftir_ldpe_soil.asp")), read_any)
+#' common <- c_spec(spectra, range = "common", res = 5)
+#' range <- c_spec(spectra, range = c(1000, 2000), res = 5)
+#'
 #' @author
 #' Zacharias Steinmetz, Win Cowger
 #'
 #' @seealso
-#' \code{\link[OpenSpecy]{conform_spec}()};
-#' \code{\link[OpenSpecy]{conform_res}()};
-#' \code{\link[OpenSpecy]{as_OpenSpecy}()};
+#' \code{\link[OpenSpecy]{conform_spec}()} for conforming wavenumbers
 #'
 #' @importFrom data.table data.table as.data.table fread rbindlist
 #'
 #' @export
-c_spec <- function(objects, wavenumber_transform = NULL, res = 5){
+c_spec <- function(x, ...) {
+  UseMethod("c_spec")
+}
 
-    if(!is.list(objects) | !all(vapply(objects, function(x){inherits(x, "OpenSpecy")}, FUN.VALUE = TRUE))){
-        stop("objects you are trying to concatenate must be a list of 'OpenSpecy' objects")
-    }  
-    
-    if(!is.null(wavenumber_transform)){
-    if(is.numeric(wavenumber_transform)){
-        new_wavenumbers = wavenumber_transform
+#' @rdname manage_spec
+#'
+#' @export
+c_spec.default <- function(x, ...) {
+  stop("object 'x' needs to be a list of 'OpenSpecy' objects")
+}
+
+#' @rdname manage_spec
+#'
+#' @export
+c_spec.OpenSpecy <- function(x, ...) {
+  warning("object 'x' needs to be a list of 'OpenSpecy' objects; ",
+          "nothing to concatenate, returning 'x'")
+
+  return(x)
+}
+
+#' @rdname manage_spec
+#'
+#' @export
+c_spec.list <- function(x, range = NULL, res = 5, ...) {
+  if(!all(vapply(x, function(y) {inherits(y, "OpenSpecy")}, FUN.VALUE = T)))
+    stop("object 'x' needs to be a list of 'OpenSpecy' objects", call. = F)
+
+  if(!is.null(range)) {
+    if(is.numeric(range)) {
+      wn <- range
     }
-    else if(wavenumber_transform == "common_range"){
-        possible_min = vapply(objects, function(x) min(x$wavenumber), FUN.VALUE = numeric(1))
-        possible_max = vapply(objects, function(x) max(x$wavenumber), FUN.VALUE = numeric(1))
-        smallest = max(possible_min)
-        largest = min(possible_max)
-        if(any(smallest > possible_max) | any(largest < possible_min)){
-            stop("Aall data points need to have some overlap in their ranges for the merge to work")
-        }
-        new_wavenumbers = c(smallest, largest)
-    }
-        objects <- lapply(objects, function(x) {
-            conform_spec(x,
-                         range = new_wavenumbers, 
-                         res = res)})
+    else if(!is.null(range) && range == "common") {
+      pmin <- vapply(x, function(y) min(y$wavenumber), FUN.VALUE = numeric(1))
+      pmax <- vapply(x, function(y) max(y$wavenumber), FUN.VALUE = numeric(1))
+
+      if(any(max(pmin) > pmax) | any(min(pmax) < pmin))
+        stop("data points need to overlap in their ranges", call. = F)
+
+      wn <- c(max(pmin), min(pmax))
     }
 
-  unlisted <- unlist(objects, recursive = F)
-
-  list <- tapply(unlisted, names(unlisted), FUN = function(x) unname((x)))
-
-  if(length(unique(vapply(list$wavenumber, length, FUN.VALUE = numeric(1)))) > 1 & is.null(wavenumber_transform)) {
-    stop("wavenumbers are not the same between spectra, you need to specify how the wavenumbers should be merged", call. = F)
+    x <- lapply(x, conform_spec, range = wn, res = res)
   }
 
-  as_OpenSpecy(
-    x = list$wavenumber[[1]],
-    spectra = as.data.table(list$spectra),
-    metadata = rbindlist(list$metadata, fill = T)[,-c("x","y")]
+  unlisted <- unlist(x, recursive = F)
+  list <- tapply(unlisted, names(unlisted), unname)
+
+  if(length(unique(vapply(list$wavenumber, length, FUN.VALUE = numeric(1)))) > 1
+     & is.null(range)) {
+    stop("wavenumbers need to be identical between spectra; specify how; use ",
+         "'range' to specify how wavenumbers should be merged", call. = F)
+  }
+
+  as_OpenSpecy(x = list$wavenumber[[1]],
+               spectra = as.data.table(list$spectra),
+               metadata = rbindlist(list$metadata, fill = T)[,-c("x","y")]
   )
 }
--- tests/testthat/test-manage_spec.R
+++ tests/testthat/test-manage_spec.R
@@ -1,19 +1,34 @@
+library(data.table)
+data("raman_hdpe")
 
+test_that("merging identical files without range specification", {
+  specs <- lapply(c(read_extdata("raman_hdpe.yml"),
+                    read_extdata("raman_hdpe.yml")), read_spec)
+  same <- c_spec(specs) |> expect_silent()
 
-test_that("Merge multiple of same file without range specification", {
-    spectra <- lapply(c(read_extdata("raman_hdpe.csv"), read_extdata("raman_hdpe.csv"), read_extdata("raman_hdpe.csv")), read_text)
-    expect_silent(spectra2 <- c_spec(objects = spectra))
-    expect_true(is_OpenSpecy(spectra2))
+  expect_equal(same$wavenumber, raman_hdpe$wavenumber)
+  expect_equal(same$spectra$intensity, raman_hdpe$spectra$intensity)
 })
 
-spectra <- lapply(c(read_extdata("raman_hdpe.csv"), read_extdata("ftir_ldpe_soil.asp")), read_any)
+specs <- lapply(c(read_extdata("raman_hdpe.yml"),
+                  read_extdata("ftir_ldpe_soil.asp")), read_any)
 
-test_that("Merge different files", {
-    expect_silent(spectra2 <- c_spec(objects = spectra, wavenumber_transform = "common_range", res = 5))
-    expect_true(is_OpenSpecy(spectra2))
+test_that("merging different files with common range", {
+  diff <- c_spec(specs, range = "common", res = 5) |>
+    expect_silent()
+
+  diff$wavenumber[1:2] |> expect_equal(c(655, 660))
+  diff$spectra$intensity[1:2] |> round(2) |> expect_equal(c(53.87, 59.00))
+  diff$spectra$intensity.1[1:2] |> round(2) |> expect_equal(c(0.03, 0.03))
 })
 
-test_that("Merge different files with specified range", {
-    expect_silent(spectra2 <- c_spec(objects = spectra, wavenumber_transform = c(1000, 2000), res = 5))
-    expect_true(is_OpenSpecy(spectra2))
+test_that("merging different files with specified range", {
+  spec <- c_spec(specs, range = c(1000, 2000), res = 5) |>
+    expect_silent()
+
+  spec$wavenumber |> expect_equal(seq(1000, 2000, 5))
+  spec$spectra$intensity |>
+    expect_equal(
+      conform_spec(raman_hdpe, c(1000, 2000), res = 5)$spectra$intensity
+      )
 })
