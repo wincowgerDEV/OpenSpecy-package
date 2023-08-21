0ab7532eb06f34bb2b9383e5e535dda3e11dbc50
Zacharias Steinmetz <git@zsteinmetz.de>
2023-08-21 12:48:04 +0200

    Prepare using S3 credential in tests

--- tests/testthat/test-share_spec.R
+++ tests/testthat/test-share_spec.R
@@ -7,7 +7,7 @@ data("raman_hdpe")
 wo_meta <- raman_hdpe
 wo_meta$metadata$spectrum_identity <- NULL
 
-test_that("share_text() gives expected output", {
+test_that("share_text() works locally", {
   share_spec(raman_hdpe, file = read_extdata("raman_hdpe.csv"), share = tmp) |>
     expect_message()
   read_extdata("testdata_zipped.zip") |> read_zip() |>
@@ -22,6 +22,20 @@ test_that("share_text() gives expected output", {
   share_spec(wo_meta, share = tmp) |>
     expect_message() |>
     expect_warning()
+})
+
+test_that("share_text() uploads to the cloud", {
+  skip_on_cran()
+  skip_if_not(testthat:::on_ci(), "Not on CI")
+  skip_if(Sys.getenv("AWS_ACCESS_KEY_ID") == "" ||
+            Sys.getenv("AWS_SECRET_ACCESS_KEY" == ""), "No credentials")
+
+  share_spec(raman_hdpe, file = read_extdata("raman_hdpe.csv"), share = "cloud",
+             credentials = list(
+               s3_key = Sys.getenv("AWS_ACCESS_KEY_ID"),
+               s3_secret = Sys.getenv("AWS_SECRET_ACCESS_KEY"),
+               s3_region = "us-east-2", s3_bucket = "openspecy")) |>
+    expect_message()
 })
 
 # Tidy up
