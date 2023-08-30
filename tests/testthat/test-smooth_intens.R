784d3180df030d7a4d9b479ba8f9d176ca817371
Win Cowger, PhD <wincowger@gmail.com>
2023-08-30 13:14:57 -0700

    update test

--- tests/testthat/test-smooth_intens.R
+++ tests/testthat/test-smooth_intens.R
@@ -4,8 +4,9 @@ test_that("smooth_intens() works as expected", {
   smt <- smooth_intens(raman_hdpe, polynomial = 3) |> expect_silent()
 
   cor(smt$spectra$intensity,
-      smooth_intens(raman_hdpe, polynomial = 1)$spectra$intensity) |> round(4) |>
-    expect_equal(0.9756, ignore_attr = F)
+      smooth_intens(raman_hdpe, polynomial = 1)$spectra$intensity) |> 
+      round(4) |>
+    expect_equal(0.8043, ignore_attr = F)
   expect_s3_class(smt, "OpenSpecy")
   expect_equal(nrow(smt$spectra), nrow(raman_hdpe$spectra))
   expect_equal(smt$wavenumber, raman_hdpe$wavenumber)
