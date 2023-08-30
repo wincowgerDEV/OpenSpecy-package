f8afebd336193214b4da4f02e4b86800990a9b1d
Zacharias Steinmetz <git@zsteinmetz.de>
2023-08-30 16:12:48 +0200

    Amend tests

--- tests/testthat/test-conform_spec.R
+++ tests/testthat/test-conform_spec.R
@@ -29,21 +29,21 @@ test_that("conform_spec() handles errors correctly", {
 test_that("conform_spec() conforms wavenumbers correctly", {
   sam <- as_OpenSpecy(wn <- seq(1000, 2000, 5),
                       data.table(intensity = rnorm(length(wn))))
-
   new_wavenumbers <- c(1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800, 1900)
-  conf_spec <- conform_spec(sam, new_wavenumbers)
-  conf_spec2 <- conform_spec(sam, new_wavenumbers,res = NULL, type = "roll")
-  
-  expect_equal(length(conf_spec$wavenumber), length(conf_spec$spectra[[1]]))
-  expect_equal(range(conf_spec$wavenumber), range(new_wavenumbers))
+  conf_new <- conform_spec(sam, new_wavenumbers) |>
+    expect_silent()
+  conf_roll <- conform_spec(sam, new_wavenumbers,res = NULL, type = "roll") |>
+    expect_silent()
 
-  expect_identical(conf_spec2$wavenumber, new_wavenumbers)
-  
-  expect_true(is_OpenSpecy(conf_spec))
-  expect_true(is_OpenSpecy(conf_spec2))
-  
+  expect_equal(length(conf_new$wavenumber), length(conf_new$spectra[[1]]))
+  expect_equal(range(conf_new$wavenumber), range(new_wavenumbers))
+
+  expect_identical(conf_roll$wavenumber, new_wavenumbers)
+
+  expect_s3_class(conf_new, "OpenSpecy")
+  expect_s3_class(conf_roll, "OpenSpecy")
+
   conform_spec(raman_hdpe)$spectra$intensity[c(63, 143, 283, 325, 402)] |>
     round(2) |>
     expect_equal(c(78.84, 65.00, 105.73, 109.41, 116.00))
 })
-
