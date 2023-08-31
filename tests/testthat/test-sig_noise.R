3c25673784df5110eb22ca5c1f16da7134620831
Zacharias Steinmetz <z.steinmetz@rptu.de>
2023-08-31 10:48:30 +0200

    Improve test coverage (#136)
    * Add secrets to codecov workflow
    * Add more tests

--- tests/testthat/test-sig_noise.R
+++ tests/testthat/test-sig_noise.R
@@ -1,7 +1,12 @@
-# Load your OpenSpecy object
 data("raman_hdpe")
 
-test_that("sig_noise returns correct values", {
+test_that("sig_noise() handles input errors corretly", {
+  sig_noise(1:1000) |> expect_error()
+  restrict_range(raman_hdpe, 300, 320) |> sig_noise() |>
+    expect_warning()
+})
+
+test_that("sig_noise() returns correct values", {
   sig_noise(raman_hdpe, metric = "sig") |> round(2) |> unname() |>
     expect_equal(101.17)
 
