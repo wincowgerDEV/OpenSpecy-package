26b781d6c5938784c0a863e27e21c86e113690ac
Zacharias Steinmetz <git@zsteinmetz.de>
2023-11-25 09:47:20 +0100

    Code styling

--- R/sig_noise.R
+++ R/sig_noise.R
@@ -8,18 +8,18 @@
 #' @param x an \code{OpenSpecy} object.
 #' @param metric character; specifying the desired metric to calculate.
 #' @param step numeric; the step size of the region to look for the run_sig_over_noise option.
-#' @param sig_min numeric; the minimum wavenumber value for the signal region
-#' @param sig_max numeric; the maximum wavenumber value for the signal region
-#' @param noise_min numeric; the minimum wavenumber value for the noise region
-#' @param noise_max numeric; the maximum wavenumber value for the noise region
+#' @param sig_min numeric; the minimum wavenumber value for the signal region.
+#' @param sig_max numeric; the maximum wavenumber value for the signal region.
+#' @param noise_min numeric; the minimum wavenumber value for the noise region.
+#' @param noise_max numeric; the maximum wavenumber value for the noise region.
 #' @param abs logical; whether to return the absolute value of the result
 #' Options include \code{"sig"} (mean intensity), \code{"noise"} (standard
 #' deviation of intensity), \code{"sig_times_noise"} (absolute value of
 #' signal times noise), \code{"sig_over_noise"} (absolute value of signal /
 #' noise), \code{"run_sig_over_noise"} (absolute value of signal /
-#' noise where signal is estimated as the max intensity and noise is 
-#' estimated as the height of a low intensity region.), 
-#' \code{"log_tot_sig"} (sum of the inverse log intensities, useful for spectra  in log units), 
+#' noise where signal is estimated as the max intensity and noise is
+#' estimated as the height of a low intensity region.),
+#' \code{"log_tot_sig"} (sum of the inverse log intensities, useful for spectra  in log units),
 #' or \code{"tot_sig"} (sum of intensities).
 #' @param na.rm logical; indicating whether missing values should be removed
 #' when calculating signal and noise. Default is \code{TRUE}.
@@ -56,38 +56,35 @@ sig_noise.default <- function(x, ...) {
 #'
 #' @export
 sig_noise.OpenSpecy <- function(x, metric = "run_sig_over_noise",
-                                na.rm = TRUE, step = 20, 
-                                sig_min = NULL, sig_max = NULL, 
+                                na.rm = TRUE, step = 20,
+                                sig_min = NULL, sig_max = NULL,
                                 noise_min = NULL, noise_max = NULL, abs = T, ...) {
-    
-  values <- vapply(x$spectra, function(y) {
 
+  values <- vapply(x$spectra, function(y) {
     if(metric == "run_sig_over_noise") {
-        if(length(y[!is.na(y)]) < step) {
-            warning(paste0("Need at least ", step, " intensity values to calculate the signal or ",
-                           "noise values accurately with run_sig_over_noise; returning NA"), call. = F)
-            return(NA)
-        }
+      if(length(y[!is.na(y)]) < step) {
+        warning(paste0("Need at least ", step, " intensity values to calculate ",
+                       "the signal or noise values accurately with ",
+                       "run_sig_over_noise; returning NA"), call. = F)
+        return(NA)
+      }
       max <- frollapply(y[!is.na(y)], step, max)
       max[(length(max) - (step-1)):length(max)] <- NA
-      signal <- max(max, na.rm = T)#/mean(x, na.rm = T)
+      signal <- max(max, na.rm = T)
       noise <- median(max[max != 0], na.rm = T)
-    }
-    else {
-        if(!is.null(sig_min) & !is.null(sig_max)){
-         sig_intens <- y[x$wavenumber >= sig_min & x$wavenumber <= sig_max]
-        }
-        else{
-            sig_intens <- y
-        }
-        if(!is.null(noise_min) & !is.null(noise_max)){
-            noise_intens <- y[x$wavenumber >= noise_min & x$wavenumber <= noise_max]
-        }
-        else{
-            noise_intens <- y
-        }
-      signal = mean(sig_intens, na.rm = na.rm)
-      noise = sd(noise_intens, na.rm = na.rm)
+    } else {
+      if(!is.null(sig_min) & !is.null(sig_max)){
+        sig_intens <- y[x$wavenumber >= sig_min & x$wavenumber <= sig_max]
+      } else {
+        sig_intens <- y
+      }
+      if(!is.null(noise_min) & !is.null(noise_max)){
+        noise_intens <- y[x$wavenumber >= noise_min & x$wavenumber <= noise_max]
+      } else {
+        noise_intens <- y
+      }
+      signal <- mean(sig_intens, na.rm = na.rm)
+      noise <- sd(noise_intens, na.rm = na.rm)
     }
 
     if(metric == "sig") return(signal)
@@ -99,11 +96,10 @@ sig_noise.OpenSpecy <- function(x, metric = "run_sig_over_noise",
     if(metric == "tot_sig") return(sum(y))
     if(metric == "log_tot_sig") return(sum(exp(y)))
   }, FUN.VALUE = numeric(1))
-  
-  if(abs){
-      return(abs(values))
-  }
-  else{
-      return(values)
+
+  if(abs) {
+    return(abs(values))
+  } else {
+    return(values)
   }
 }
