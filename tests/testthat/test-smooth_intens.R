fd3b06a6ff441726a9d5f549f60aaaa78970fc70
Win Cowger, PhD <wincowger@gmail.com>
2024-01-25 19:42:21 -0800

    add wh tests

--- tests/testthat/test-smooth_intens.R
+++ tests/testthat/test-smooth_intens.R
@@ -25,6 +25,15 @@ test_that("smooth_intens() works as expected", {
       smooth_intens(raman_hdpe, polynomial = 1)$spectra$intensity) |>
       round(4) |>
     expect_equal(0.8043, ignore_attr = F)
+  
+  smt2 <- smooth_intens(raman_hdpe, lambda = 1600, d = 2, lag = 2, type = "wh") |> expect_silent()
+  
+  cor(smt2$spectra$intensity,smooth_intens(raman_hdpe, lambda = 100, d = 2, lag = 2, type = "wh")$spectra$intensity) |>
+      round(4) |>
+      expect_equal(0.8572, ignore_attr = F)
+  
+  expect_true(check_OpenSpecy(smt2))
+  expect_true(check_OpenSpecy(smt))
   expect_s3_class(smt, "OpenSpecy")
   expect_equal(nrow(smt$spectra), nrow(raman_hdpe$spectra))
   expect_equal(smt$wavenumber, raman_hdpe$wavenumber)
