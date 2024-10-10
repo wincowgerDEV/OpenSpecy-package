0033025e0ac3f8de7352c51da199977f3c9bd922
Win Cowger, PhD <wincowger@gmail.com>
2024-10-10 07:26:46 -1000

    try to fix lib read warnings

--- tests/testthat/test-manage_lib.R
+++ tests/testthat/test-manage_lib.R
@@ -2,22 +2,13 @@
 tmp <- file.path(tempdir(), "OpenSpecy-testthat")
 dir.create(tmp, showWarnings = F)
 
-#test_that("stop if OSF not reachable", {
-#  skip_on_cran()
-  #skip_if_not(is.null(curl::nslookup("api.osf.io", error = F)),
-  #            message = "OSF is online") #Curl doesn't work nicely with shinylive
-
-#  get_lib(type = "test", path = tmp) |>
-#    expect_error()
-#})
-
-
 
 test_that("check_lib() finds complete library", {
   skip_on_cran()
   skip_if_offline(host = "api.osf.io")
   skip_if_not(testthat:::on_ci(), "Not on CI")
 
+  get_lib(type = c("derivative", "nobaseline"), path = tmp)
   check_lib(type = c("derivative", "nobaseline"), path = tmp) |>
     expect_silent()
   check_lib(type = "raw", path = tmp) |>
