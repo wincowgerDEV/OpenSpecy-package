9fe0d18322803f959e7fede5c0ad8336bc09a2ef
Win Cowger, PhD <wincowger@gmail.com>
2024-12-03 17:51:45 -0800

    lots of new baseline options.

--- R/subtr_baseline.R
+++ R/subtr_baseline.R
@@ -13,7 +13,8 @@
 #' from the S-Modpoly algorithm (\url{https://github.com/jackma123-rgb/S-Modpoly}),
 #' and \code{"manual"} for applying a user-provided baseline.
 #'
-#' @param x a list object of class \code{OpenSpecy}.
+#' @param x a list object of class \code{OpenSpecy} or a vector of wavenumbers.
+#' @param y a vector of spectral intensities.
 #' @param type one of \code{"polynomial"} or \code{"manual"} depending on
 #' the desired baseline correction method.
 #' @param degree the degree of the full spectrum polynomial for \code{"imodpoly"} and \code{"smodpoly"}. Must be less than the number of
@@ -22,11 +23,17 @@
 #' @param degree_part the degree of the polynomial for \code{"smodpoly"}. Must be less than the number of
 #' unique points.
 #' @param iteration the number of iterations for \code{"smodpoly"} baseline correction.
-#' @param peak_width_mult scaling factor for the width of peak detection regions in \code{"smodpoly"}.
-#' @param peak_height_mult scaling factor for the height of peak detection regions in \code{"smodpoly"}.
+#' @param peak_width_mult scaling factor for the width of peak detection regions.
+#' @param termination_diff scaling factor for the ratio of difference in residual standard deviation to terminate iterative fitting with.
 #' @param raw if \code{TRUE}, use raw and not orthogonal polynomials for \code{"imodpoly"}.
+#' @param full logical, whether to use the full spectrum or to partition. 
+#' @param remove_peaks logical, whether to remove peak regions during first iteration. 
+#' @param refit_at_end logical, whether to refit a polynomial to the end result (TRUE) or to use linear approximation.
+#' @param crop_boundaries logical, whether to smartly crop the boundaries to match the spectra based on peak proximity.
 #' @param baseline an \code{OpenSpecy} object containing the baseline data to be
 #' subtracted (only for \code{"manual"}).
+#' @param bl_x a vector of wavenumbers for the baseline. 
+#' @param bl_y a vector of spectral intensities for the baseline. 
 #' @param make_rel logical; if \code{TRUE}, spectra are automatically normalized
 #' with \code{\link{make_rel}()}.
 #' @param \ldots further arguments passed to \code{\link[stats]{poly}()} or \code{"smodpoly"} parameters.
@@ -79,11 +86,15 @@ subtr_baseline <- function(x, ...) {
 #' @rdname subtr_baseline
 #'
 #' @export
-subtr_baseline.default <- function(x,type = "polynomial",
+subtr_baseline.default <- function(x,y,type = "polynomial",
                                    degree = 8, raw = FALSE,
+                                   full = T,
+                                   remove_peaks = T,
+                                   refit_at_end = F,
+                                   crop_boundaries = F,
                                    iterations = 10,  
                                    peak_width_mult = 3,
-                                   peak_height_mult = 0.05,
+                                   termination_diff = 0.05,
                                    degree_part = 2, 
                                    bl_x, bl_y, make_rel = TRUE, ...) {
     if(type == "polynomial"){
@@ -97,148 +108,142 @@ subtr_baseline.default <- function(x,type = "polynomial",
         paramVector <- lm(y ~ stats::poly(x, degree = degree, raw = raw))
         mod_poly <- paramVector$fitted.values
         dev_curr <- sd(paramVector$residuals)
-        peaks <- y > mod_poly + dev_curr
-        y <- y[!peaks]
-        mod_poly <- mod_poly[!peaks]
-        x <- x[!peaks]
-        while (!criteria_met) {
-            # Predict the intensity using the polynomial of specified length
-            paramVector <- lm(y ~ stats::poly(x, degree = degree, raw = raw))
-            mod_poly <- paramVector$fitted.values
-            dev_curr <- sd(paramVector$residuals)
-
-            y <- pmin(mod_poly + dev_curr, y)
+        if(!full){
+            # Step 2: Detect intersections
+            left_intersection <- which(diff(mod_poly + dev_curr >= y) == -1)
+            right_intersection <- which(diff(mod_poly + dev_curr <= y) == -1)
             
-            # Test criteria
-            it = it + 1
-            if(!is.null(iteration)){
-                criteria_met <- abs((dev_curr - dev_prev) / dev_curr) <= 0.05 | it == iteration
+            # Ensure boundaries are handled correctly
+            if (length(right_intersection) == 0 || length(left_intersection) == 0) {
+                left_intersection <- c(1, left_intersection)
+                right_intersection <- c(right_intersection, pixels_number)
+            }
+            
+            # Ensure boundaries are handled correctly
+            if (min(right_intersection) < min(left_intersection)) {
+                left_intersection <- c(1, left_intersection)
+            }
+            
+            # Ensure boundaries are handled correctly
+            if (max(right_intersection) < max(left_intersection)) {
+                right_intersection <- c(right_intersection, pixels_number)
+            }
+            
+            # Step 3: Compute original width and peak locations
+            #max peak width
+            or_width <- max(right_intersection - left_intersection)
+            #keeping because could be useful one day
+            #peak location
+            opeaks <- round((right_intersection + left_intersection) / 2)
+            #height difference at peak locations
+            #ipeaks <- y[opeaks] - setarray[opeaks]
+            #max height difference
+            #mpeaks <- max(ipeaks)
+        }
+        if(remove_peaks){
+            peaks <- y > mod_poly + dev_curr
+            y <- y[!peaks]
+            mod_poly <- mod_poly[!peaks]
+            x <- x[!peaks]            
+        }
+
+        if(!full){
+            pixels_number <- length(x)
+            # Step 4: Initialize boundaries for iterative fitting
+            if(peak_width_mult * or_width <= degree_part) {
+                step_size <- degree_part + 1 
             }
             else{
-                criteria_met <- abs((dev_curr - dev_prev) / dev_curr) <= 0.05
+                step_size <- peak_width_mult * or_width
             }
+            left_boundary <- 1
+            right_boundary <- step_size
+            adjusted_spectrum <- numeric(length(x))
             
-            # Approximate the intensity back to the original wavelengths, allows below
-            # the peak to be interpolated
-            if(criteria_met) {
-                ysbg <- approx(x, y, xout = xout, rule = 2, method = "linear",
-                               ties = mean)[2] |>
-                    unlist() |>
-                    unname()
+            # Helper function for iterative fitting
+            fit_function <- function(start, end, iter, spectrum, order) {
+                seg_x <- seq(start, end)
+                seg_y <- spectrum[start:end]
                 
-                return(yin - ysbg)
+                if(length(seg_y) <= order) return(seg_y)
+                
+                for (i in seq_len(iter)) {
+                    p <- lm(seg_y ~ poly(seg_x, order, raw = raw))
+                    py_fit <- predict(p, newdata = data.frame(seg_x = seg_x))
+                    dev_curr <- sd(p$residuals)
+                    seg_y <- pmin(seg_y, py_fit + dev_curr)
+                }
+                
+                seg_y
             }
             
-            # Update previous residual metric
-            dev_prev <- dev_curr
+            # Step 5: Iterative fitting
+            while (left_boundary < pixels_number) {
+                if (right_boundary >= pixels_number) {
+                    right_boundary <- pixels_number
+                }
+                
+                # Fit the segment
+                fit_array <- fit_function(start = left_boundary, end = right_boundary, iter = iteration, spectrum = y, order = degree_part)
+                adjusted_spectrum[left_boundary:right_boundary] <- fit_array
+                
+                left_boundary <- right_boundary + 1
+                right_boundary <- min(left_boundary + step_size, pixels_number)
+            }
         }
-    }
-        if(type == "manual"){
-            y - approx(bl_x, bl_y, xout = x, rule = 2, method = "linear", ties = mean)$y
-        }
-
-    
-    .subtr_bl_smod <- function(y, x, iteration, peak_width_mult, peak_height_mult, degree_full, degree_part) {
-
-        # Step 2: Detect intersections
-        left_intersection <- which(diff(setarray >= y) == -1)
-        right_intersection <- which(diff(setarray <= y) == -1)
-        
-        # Ensure boundaries are handled correctly
-        if (length(right_intersection) == 0 || length(left_intersection) == 0) {
-            left_intersection <- c(1, left_intersection)
-            right_intersection <- c(right_intersection, pixels_number)
-        }
-        
-        # Ensure boundaries are handled correctly
-        if (min(right_intersection) < min(left_intersection)) {
-            left_intersection <- c(1, left_intersection)
-        }
-        
-        # Ensure boundaries are handled correctly
-        if (max(right_intersection) < max(left_intersection)) {
-            right_intersection <- c(right_intersection, pixels_number)
-        }
-        
-        # Step 3: Compute original width and peaks
-        #max peak width
-        or_width <- max(right_intersection - left_intersection)
-        #peak location
-        opeaks <- round((right_intersection + left_intersection) / 2)
-        #height difference at peak locations
-        ipeaks <- y[opeaks] - setarray[opeaks]
-        #max height difference
-        mpeaks <- max(ipeaks)
-        #actual peak locations
-        peaks <- opeaks[ipeaks > peak_height_mult * mpeaks]
-        
-        dev_curr <- sd(p$residuals)
-        
-        # Remove peaks
-        peak_points <- y > setarray + dev_curr
-        y <- pmin(setarray + dev_curr, y)[!peak_points]
-        x <- x[!peak_points]
-        pixels_number <- length(x)
-        # Step 4: Initialize boundaries for iterative fitting
-        if(peak_width_mult * or_width <= degree_part) {
-            step_size <- degree_part + 1 
-        }
+       
         else{
-            step_size <- peak_width_mult * or_width
-        }
-        left_boundary <- 1
-        right_boundary <- step_size
-        adjusted_spectrum <- numeric(length(x))
-        
-        # Helper function for iterative fitting
-        fit_function <- function(start, end, iter, spectrum, order) {
-            seg_x <- seq(start, end)
-            seg_y <- spectrum[start:end]
-            
-            if(length(seg_y) <= order) return(seg_y)
-            
-            for (i in seq_len(iter)) {
-                p <- lm(seg_y ~ poly(seg_x, order))
-                py_fit <- predict(p, newdata = data.frame(seg_x = seg_x))
-                dev_curr <- sd(p$residuals)
-                seg_y <- pmin(seg_y, py_fit + dev_curr)
+            while (!criteria_met) {
+                # Predict the intensity using the polynomial of specified length
+                paramVector <- lm(y ~ stats::poly(x, degree = degree, raw = raw))
+                mod_poly <- paramVector$fitted.values
+                dev_curr <- sd(paramVector$residuals)
+                
+                y <- pmin(mod_poly + dev_curr, y)
+                
+                # Test criteria
+                if(!is.null(iteration)){
+                    criteria_met <- abs((dev_curr - dev_prev) / dev_curr) <= termination_diff | it == iteration
+                }
+                else{
+                    criteria_met <- abs((dev_curr - dev_prev) / dev_curr) <= termination_diff
+                }
+                
+                # Update previous residual metric
+                dev_prev <- dev_curr
             }
-            
-            seg_y
-        }
-        
-        # Step 5: Iterative fitting
-        while (left_boundary < pixels_number) {
-            if (right_boundary >= pixels_number) {
-                right_boundary <- pixels_number
-            }
-            
-            # Fit the segment
-            fit_array <- fit_function(start = left_boundary, end = right_boundary, iter = iteration, spectrum = y, order = degree_part)
-            adjusted_spectrum[left_boundary:right_boundary] <- fit_array
-            
-            left_boundary <- right_boundary + 1
-            right_boundary <- min(left_boundary + step_size, pixels_number)
         }
         
         # Step 6: Final adjustment and fitting a new polynomial to the baseline to smooth
-        final_fit <- lm(adjusted_spectrum ~ poly(x, degree_full))
-        final_baseline <- predict(final_fit, newdata = data.frame(x = or_x))
-        
+        if(refit_at_end){
+            final_fit <- lm(y ~ poly(x, degree_full))
+            final_baseline <- predict(final_fit, newdata = data.frame(x = xout))            
+        }
+        else{
+            final_baseline <- approx(x, y, xout = xout, rule = 2, method = "linear",
+                   ties = mean)[2] |>
+                unlist() |>
+                unname()
+        }
+
         if(crop_boundaries){
             pixels_number <- length(final_baseline)
             # # Step 7: Correct boundaries peaks
-             if (pixels_number - max(peaks) >= or_width) {
-                 final_baseline[(max(peaks) + floor(0.5 * or_width)):pixels_number] <- or_y[(max(peaks) + floor(0.5 * or_width)):pixels_number]
-             }
-             if (min(peaks) >= or_width) {
-                 final_baseline[1:floor(0.5 * or_width)] <- or_y[1:floor(0.5 * or_width)]
-             }            
+            if (pixels_number - max(opeaks) >= or_width) {
+                final_baseline[(max(opeaks) + floor(0.7 * or_width)):pixels_number] <- yin[(max(opeaks) + floor(0.7 * or_width)):pixels_number]
+            }
+            if (min(opeaks) >= or_width) {
+                final_baseline[1:floor(0.3 * or_width)] <- yin[1:floor(0.3 * or_width)]
+            }            
         }
-
-        
-        return(or_y - final_baseline)
+        corrected = yin - final_baseline
     }
+        if(type == "manual"){
+            corrected = y - approx(bl_x, bl_y, xout = x, rule = 2, method = "linear", ties = mean)$y
+        }
+    
+    if (make_rel) make_rel(corrected) else corrected
+    
 }
 
 #' @rdname subtr_baseline
@@ -246,31 +251,33 @@ subtr_baseline.default <- function(x,type = "polynomial",
 #' @export
 subtr_baseline.OpenSpecy <- function(x, type = "polynomial",
                                      degree = 8, raw = FALSE,
-                                     iteration = 10,  
+                                     full = T,
+                                     remove_peaks = T,
+                                     refit_at_end = F,
+                                     crop_boundaries = F,
+                                     iterations = 10,  
                                      peak_width_mult = 3,
-                                     peak_height_mult = 0.05,
+                                     termination_diff = 0.05,
                                      degree_part = 2, 
                                      baseline, make_rel = TRUE, ...) {
-    if (type == "imodpoly") {
-        sbg <- x$spectra[, lapply(.SD, subtr_baseline, x = x$wavenumber,
-                                  degree = degree, iteration = iteration, raw = raw, ...)]
-    } else if (type == "manual") {
-        if (!is_OpenSpecy(baseline))
-            stop("'baseline' needs to be of class 'OpenSpecy'", call. = F)
-        sbg <- x$spectra[, lapply(.SD, .subtr_bl_manual, x = x$wavenumber,
-                                  bl_y = baseline$spectra[[1]],
-                                  bl_x = baseline$wavenumber, ...)]
-    } else if (type == "smodpoly") {
-        sbg <- x$spectra[, lapply(.SD, .subtr_bl_smod, x = x$wavenumber,
-                                  iteration = iteration, peak_width_mult = peak_width_mult, 
-                                  peak_height_mult = peak_height_mult, 
-                                  degree_full = degree, degree_part = degree_part, ...)]
-    } else {
-        stop("'type' must be either 'polynomial', 'manual', or 'smodpoly'", call. = F)
-    }
-    
-    if (make_rel) x$spectra <- sbg[, lapply(.SD, make_rel)] else x$spectra <- sbg
-    
+     if (type == "manual" & !is_OpenSpecy(baseline)) stop("'baseline' needs to be of class 'OpenSpecy'", call. = F)
+        sbg <- x$spectra[, lapply(.SD, function(y){
+            subtr_baseline(x = x$wavenumber,
+                           y = y,
+                           type = "polynomial",
+                           degree = degree, 
+                           raw = raw,
+                           full = full,
+                           remove_peaks = remove_peaks,
+                           refit_at_end = refit_at_end,
+                           crop_boundaries = crop_boundaries,
+                           iterations = iterations,  
+                           peak_width_mult = peak_width_mult,
+                           termination_diff = termination_diff,
+                           degree_part = degree_part, 
+                           bl_x = baseline$wavenumber, 
+                           bl_y = baseline$spectra[[1]], 
+                           make_rel = make_rel, ...)})]
     return(x)
 }
 
