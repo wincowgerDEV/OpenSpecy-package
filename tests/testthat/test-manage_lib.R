b0bc1b5cd797a2ad763c23b0b53f14e4dcf39b73
Win Cowger, PhD <wincowger@gmail.com>
2025-04-18 17:28:54 -0700

    better tests

--- tests/testthat/test-manage_lib.R
+++ tests/testthat/test-manage_lib.R
@@ -7,17 +7,21 @@ test_that("check_lib() finds complete library", {
   skip_on_cran()
   skip_if_offline(host = "api.osf.io")
   skip_if_not(testthat:::on_ci(), "Not on CI")
-
-  get_lib(type = c("derivative", "nobaseline"), path = tmp, aws = TRUE)
-  check_lib(type = c("derivative", "nobaseline"), path = tmp) |>
+  
+  get_lib(type = c("derivative"), path = tmp, aws = TRUE, revision = "DtlNCNhOGdtCJqRCN3F8jRs732t_yZLM")
+  check_lib(type = c("derivative"), path = tmp) |>
+      expect_silent()
+  get_lib(type = c("nobaseline"), path = tmp, aws = TRUE)
+  check_lib(type = c("nobaseline"), path = tmp) |>
+      expect_silent()
+  get_lib(type = c("medoid_derivative"), path = tmp)
+  check_lib(type = c("medoid_derivative"), path = tmp) |>
+    expect_silent()
+  get_lib(type = c("medoid_nobaseline"), path = tmp, revision = 1)
+  check_lib(type = c("medoid_nobaseline"), path = tmp) |>
       expect_silent()
   check_lib(type = "raw", path = tmp) |>
       expect_warning()
-  get_lib(type = c("derivative", "nobaseline"), path = tmp)
-  check_lib(type = c("derivative", "nobaseline"), path = tmp) |>
-    expect_silent()
-  check_lib(type = "raw", path = tmp) |>
-    expect_warning()
 })
 
 test_that("load_lib() works with complete library", {
