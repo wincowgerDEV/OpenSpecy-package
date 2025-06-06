3b7824cfa1927cc04358e1d4457f7b09a92db491
Win Cowger, PhD <wincowger@gmail.com>
2025-06-05 17:38:34 -0700

    area under the band update

--- R/area_under_band.R
+++ R/area_under_band.R
@@ -11,6 +11,7 @@
 #' @param min a numeric value of the smallest wavenumber to begin calculation.
 #' @param max a numeric value of the smallest wavenumber to begin calculation.
 #' @param na.rm a logical value for whether to ignore NA values. 
+#' @param \ldots additional arguments passed to vapply.
 #'
 #' @return
 #' Numeric vector of area under the band calculations for each spectrum in the Open Specy object. 
@@ -18,9 +19,9 @@
 #' @examples
 #' data("raman_hdpe")
 #' #Single area calculation
-#' area_under_band(raman_hdpe, c(1000, 2000))
+#' area_under_band(raman_hdpe, min = 1000,max = 2000)
 #' #Ratio of two areas. 
-#' area_under_band(raman_hdpe, c(1000, 2000))/area_under_band(raman_hdpe, c(500, 700))
+#' area_under_band(raman_hdpe, min = 1000,max = 2000)/area_under_band(raman_hdpe, min = 500,max = 700)
 #' @author
 #' Win Cowger
 #'
@@ -41,8 +42,8 @@ area_under_band.default <- function(x, ...) {
 #' @rdname area_under_band
 #'
 #' @export
-area_under_band.OpenSpecy <- function(x, min, max, na.rm = F) {
+area_under_band.OpenSpecy <- function(x, min, max, na.rm = F, ...) {
     logic <- x$wavenumber >= min & x$wavenumber <= max
-    vapply(x$spectra, function(x){sum(x[logic])}, FUN.VALUE = numeric(1))
+    vapply(x$spectra, function(x){sum(x[logic])}, FUN.VALUE = numeric(1), ...)
 }
 
